b675126b09915e2ef84d90e96e7df9daf766551b
AnyiDc <anyi.katerine@hotmail.com>
2025-03-11 20:53:01 -0500

    feat se crea rutina de busqueda y edicion de productos

--- main.py
+++ main.py
@@ -33,9 +33,30 @@ while opcion != 5:
         
         
     elif opcion==2:
-        print("estoy en la 2")
+
+        "utilizando ciclo for en python para recorrer listas 11032025"
+        for producto in productos:
+            print(producto["nombre"])
     elif opcion==3:
         print("estoy en la 3")
+        #modificar un producto
+        #0. PREGUNTAR A QUIEN QUIERO EDITAR
+        productoCambio=int(input("Digite el ID del producto a cambiar:"))
+        #1. ENCONTRAR EL ELEMENTO
+        for productoBuscado in productos:
+            if productoBuscado["id"]==productoCambio:
+                print("Producto encontrado")
+            else:
+                print("Producto no encontrado")
+                #2. MODIFICO LOS VALORES
+                productoBuscado["nombre"]=input("Digita el nuevo nombre del producto: ")
+                productoBuscado["precio"]=int(input("Digita el nuevo precio del producto: "))
+                productoBuscado["cantidad"]=int(input("Cuantos elementos de este producto vas a llevar: "))
+                productoBuscado["presentacion"]=input("Cual presentacion llevaras? ")
+                print("Producto modificado correctamente")
+                break
+        #2. SELECCIONO EL ELEMENTO 
+        #3. ACCEDO A LSA PROPIEDADES O ATRIBUTOS QUE QUIERO O PUEDO MODIFICAR
     elif opcion==4:
         print("estoy en la 4")
     else:
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Hector Alejandro Gaviria Marin
+## Examen 1 de python (fundamentos logicos)
+### Medellin 2025
+#### Docente: MR Gallego
